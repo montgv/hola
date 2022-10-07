5c5b1d5d3390fe5981a0ffaacdaecad51decab05
estudiante <montserrat.garrido.220@estudiante.iesoretania.es>
2022-10-07 11:10:54 +0200

    Initial commit

---
+++ core/src/main/java/es/iesoretania/dam2/hlc/HolaLibgdx.java
@@ -0,0 +1,36 @@
+package es.iesoretania.dam2.hlc;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class HolaLibgdx extends ApplicationAdapter {
+	private SpriteBatch batch;
+	private BitmapFont font;
+
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		font.setColor(Color.YELLOW);
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		font.draw(batch, "¡Hola Mundo!", 20, 30);
+		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
+	}
+}
